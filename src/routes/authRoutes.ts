cad65e29f6569b24fd168da380fc2b3e90374de2
Fátima Kraiczyi <fatimakraiczyi@gmail.com>
2025-11-13 00:38:55 -0300

    feat: remove endpoint '/me' do controlador de autenticação

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,8 @@
 import { Router } from 'express';
-import { login, logout, refresh, reset, me } from '../controllers/authController.js';
+import { login, logout, refresh, reset } from '../controllers/authController.js';
 
 export const authRouter = Router();
 authRouter.post('/login', login);
 authRouter.post('/logout', logout);
 authRouter.post('/refresh', refresh);
 authRouter.post('/reset-password', reset);
-authRouter.get('/me', me);
